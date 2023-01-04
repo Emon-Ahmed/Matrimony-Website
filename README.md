3fde7e5a2f7904834c42518225cb88da1f19a1c4
Emon Ahmed <technicalboyemon@gmail.com>
2023-01-04 22:02:31 +0600

    SETUP NEXTJS WEB APP

---
+++ README.md
@@ -0,0 +1,9 @@
+# Matrimony-Website
+
+## Getting Started
+
+First, install & run the development server:
+
+```bash
+npm i & npm run dev
+```
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Matrimony Website</title>
+        <meta name="description" content="Matrimony Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <h1>Matrimony Website</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
